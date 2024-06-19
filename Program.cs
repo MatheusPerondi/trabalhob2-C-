bed5385881d700582a532e0418aa85b30c045254
Matheus Perondi <166682965+MatheusPerondi@users.noreply.github.com>
2024-06-19 01:36:46 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Trabalhob2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Agricultural wheat = new Agricultural("Wheat", 100.00, 150);
+            Agricultural sugarCane = new Agricultural("Sugar Cane", 50, 100);
+            Agricultural coffe = new Agricultural("Coffe ", 20, 170);
+            Agricultural soy = new Agricultural("Soy", 10, 210);
+
+            Industrialized chips = new Industrialized("Chips", 15, 32);
+            Industrialized cookie = new Industrialized("Cookie", 17, 190);
+            Industrialized pizza = new Industrialized("Pizza", 43, 110);
+            Industrialized cheese = new Industrialized("Cheese", 25, 60);
+
+
+            List<Product> products = new List<Product>();
+            products.Add(sugarCane);
+            products.Add(coffe);
+            products.Add(soy);
+            products.Add(chips);
+            products.Add(cookie);
+            products.Add(pizza);
+            products.Add(cheese);
+        }
+    }
+}
